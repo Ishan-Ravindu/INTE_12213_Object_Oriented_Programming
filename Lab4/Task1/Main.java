0253ff17d6cc1e7d26d101bade0c2f2277e9bd14
Ishan-Ravindu <ishanravindu27596@gmail.com>
2021-12-26 15:11:33 +0530

    finish Lab4 Task1

---
+++ Lab4/Task1/Main.java
@@ -0,0 +1,21 @@
+package Lab4.Task1;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 8; i++) {
+            if (i % 2 == 0) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 8; j++) {
+                System.out.print("*");
+                System.out.print(" ");
+            }
+
+            System.out.println("");
+        }
+    }
+}
